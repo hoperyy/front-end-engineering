28274aebefa562756c5d67a6fa3e0afa0c1fc499
liuyuanyang <liuyuanyang@weidian.com>
2017-08-25 17:52:07 +0800

    add scaffold demo

---
+++ scaffold-demo/gulpfile.js
@@ -0,0 +1,44 @@
+
+function configDevServer({ srcDir, buildDir, debugPort }) {
+
+    const webpack = require('webpack');
+    const webpackDevServer = require('webpack-dev-server');
+
+    const webpackConfig = require('./webpack.config')({ srcDir, buildDir });
+
+    webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    for(let k in webpackConfig.entry) {
+        webpackConfig.entry[k].unshift(`webpack-dev-server/client?http://127.0.0.1:${debugPort}`, 'webpack/hot/dev-server');
+    }
+
+    console.log('\nwebpack: Compiling...');
+
+    const server = new webpackDevServer(webpack(webpackConfig), {
+        contentBase: buildDir,
+        hot: true,
+        historyApiFallback: true,
+        quiet: false,
+        noInfo: false,
+        stats: {
+            chunks: false,
+            colors: true
+        },
+        publicPath: webpackConfig.output.publicPath,
+        disableHostCheck: true,
+        watchOptions: {
+            ignored: /\/node_modules\//,
+            poll: 300
+        }
+    });
+
+    return server;
+}
+
+module.exports = ({ srcDir, buildDir, currentEnv, debugPort }) => {
+
+    const server = configDevServer({ srcDir, buildDir, debugPort });
+
+    server.listen(debugPort);
+
+};
---
+++ scaffold-demo/webpack.config.js
@@ -0,0 +1,68 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const StringReplacePlugin = require('string-replace-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = ({ srcDir, buildDir }) => {
+    return {
+        entry: {
+            build: [path.join(srcDir, 'main.js')],
+            vendor: ['vue', 'vue-router']
+        },
+        output: {
+            path: path.join(buildDir, 'static'), // 必须是绝对路径
+            publicPath: '/static/',
+            filename: '[name].js'
+        },
+        resolve: {
+          extensions: ['.js', '.vue'],
+          alias: {
+              'vue': 'vue/dist/vue.js'
+          }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    use: ['vue-loader']
+                },
+                {
+                    test: /\.css$/,
+                    use: ['vue-style-loader', 'css-loader', 'postcss-loader']
+                },
+                {
+                    test: /\.less$/,
+                    use: ['vue-style-loader', 'css-loader', 'postcss-loader', 'less-loader']
+                },
+                {
+                    test: /\.js$/,
+                    use: 'babel-loader',
+                    include: [__dirname + '/src']
+                },
+
+                // 用于配置关键词替换
+                {
+                    test: /\.[(vue)(vuex)(js)(jsx)(html)]*$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: StringReplacePlugin.replace({
+                        replacements: [{
+                              pattern: new RegExp('$_TEST_REPLACE_$'.replace(/\$/g, '\\$', 'g')),
+                              replacement: function (match, p1, offset, string) {
+                                  return 'this is a test replace demo string';
+                              }
+                          }]
+                    })
+                }
+
+            ]
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor',
+                filename: 'common.js'
+            })
+        ]
+    };
+};
