7503469b222f1f4ee32df5199855caa4f2aa34ad
hoperyy <antfootmail@gmail.com>
2018-05-30 00:04:25 +0800

    update

--- build.js
+++ build.js
@@ -1,3 +1,3 @@
 
 
-require('generate-docs-by-github-issue')({ username: 'hoperyy', repo: 'front-end-engineering', targetDir: require('path').join(process.cwd(), 'cacheDocs') });
+// require('generate-docs-by-github-issue')({ username: 'hoperyy', repo: 'front-end-engineering', targetDir: require('path').join(process.cwd(), 'cacheDocs') });
