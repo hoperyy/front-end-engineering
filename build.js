46434eae0d2e75c3b7b1cc8f552e4ea14cac7b01
hoperyy <antfootmail@gmail.com>
2018-05-30 10:27:19 +0800

    简化 build 逻辑

--- build.js
+++ build.js
@@ -1,3 +1,26 @@
 
 
-// require('generate-docs-by-github-issue')({ username: 'hoperyy', repo: 'front-end-engineering', targetDir: require('path').join(process.cwd(), 'cacheDocs') });
+const path = require('path');
+const cwd = process.cwd();
+
+// require('child_process').execSync(`cd ${cwd} && rm -rf docs`);
+
+const sortedDirArr = [];
+
+require('generate-docs-by-github-issue')({
+    username: 'hoperyy',
+    repo: 'front-end-engineering',
+    targetDir: path.join(cwd, 'cached-docs'),
+    beforeSort(issues) {
+        issues.forEach(issueItem => {
+            // 去掉标题中的 / 字符和空格
+            issueItem.title = issueItem.title.replace(/\//g, '-').replace(/\s/g, '').replace(/(\[)|(\])/g, '-');
+        });
+    },
+
+    afterSort(issues) {
+        issues.forEach(issueItem => {
+            console.log(`+  [${issueItem.title}](${issueItem.html_url})`);
+        });
+    }
+});
